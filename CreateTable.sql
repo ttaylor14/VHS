8dab35e4beafad08f5243cdd768740e1f261f7c1
Todd <tjtaylor1@mail.lipscomb.edu>
2019-03-10 16:52:07 -0500

    Table Update

--- CreateTable.sql
+++ CreateTable.sql
@@ -2,7 +2,8 @@ CREATE TABLE Customer
 (
   CustomerID    INTEGER,
   NameF         TEXT,
-  NameL         TEXT
+  NameL         TEXT,
+  Address       TEXT
 );
 
 CREATE TABLE Membership
@@ -10,11 +11,34 @@ CREATE TABLE Membership
   CustomerID    INTEGER,
   ThemeID       INTEGER,
   StartDate         TEXT,
-  EndDate         TEXT
+  EndDate         TEXT,
+  PricePYR      REAL,
+  PricePMO      REAL
 );
 
 CREATE TABLE Billing
 (
-  CustomerID    INTEGER
+  CustomerID    INTEGER,
+  PaymentMethod TEXT,
+  BillingCycle  TEXT,
+  PaymentStatus TEXT,
+  CurrentBalance REAL,
+
 );
 
+CREATE TABLE Movie
+(
+  MovieID    INTEGER,
+  MovieName  TEXT,
+  ReleaseDate Date,
+  MovieDirector TEXT
+);
+
+
+CREATE TABLE Customer_History
+(
+  MovieID    INTEGER
+  DatePurchased Date,
+  ThemeID     INTEGER,
+  Price       Real
+);
