e30c5a2cdb65eb587aea69dec94960392ad6b0db
Todd <tjtaylor1@mail.lipscomb.edu>
2019-03-10 16:54:12 -0500

    Adjust Spacing

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,37 +1,37 @@
 CREATE TABLE Customer
 (
-  CustomerID    INTEGER,
-  NameF         TEXT,
-  NameL         TEXT,
-  Address       TEXT
+  CustomerID      INTEGER,
+  NameF           TEXT,
+  NameL           TEXT,
+  Address         TEXT
 );
 
 CREATE TABLE Membership
 (
-  CustomerID    INTEGER,
-  ThemeID       INTEGER,
-  StartDate         TEXT,
+  CustomerID      INTEGER,
+  ThemeID         INTEGER,
+  StartDate       TEXT,
   EndDate         TEXT,
-  PricePYR      REAL,
-  PricePMO      REAL
+  PricePYR        REAL,
+  PricePMO        REAL
 );
 
 CREATE TABLE Billing
 (
-  CustomerID    INTEGER,
-  PaymentMethod TEXT,
-  BillingCycle  TEXT,
-  PaymentStatus TEXT,
-  CurrentBalance REAL,
+  CustomerID      INTEGER,
+  PaymentMethod   TEXT,
+  BillingCycle    TEXT,
+  PaymentStatus   TEXT,
+  CurrentBalance  REAL,
 
 );
 
 CREATE TABLE Movie
 (
-  MovieID    INTEGER,
-  MovieName  TEXT,
-  ReleaseDate Date,
-  MovieDirector TEXT
+  MovieID         INTEGER,
+  MovieName       TEXT,
+  ReleaseDate     Date,
+  MovieDirector   TEXT
 );
 
 
